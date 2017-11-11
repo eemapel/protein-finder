10b7df4f428bb9317410f35a4589f17b7724c60b
Marko Pellikka <eemapel@gmail.com>
2017-11-11 19:20:42 +0000

    Encrypt passwords

--- web-app/models/user.js
+++ web-app/models/user.js
@@ -1,5 +1,7 @@
+var bcrypt = require('bcryptjs');
 var mongoose = require('mongoose');
 
+// TODO: Use docker alias
 mongoose.connect('mongodb://localhost/proteinfinder');
 
 var db = mongoose.connection;
@@ -18,6 +20,11 @@ var UserSchema = mongoose.Schema({
 var User = module.exports = mongoose.model('User', UserSchema);
 
 module.exports.createUser = function(newUser, callback) {
-  newUser.save(callback);
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(newUser.password, salt, function(err, hash) {
+      newUser.password = hash;
+      newUser.save(callback);
+    });
+  });  
 };
 
