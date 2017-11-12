89eebb6832704bf6ec4b66a43d55c56015702928
Marko Pellikka <eemapel@gmail.com>
2017-11-12 05:15:17 +0000

    Templating to show mocked search results

---
+++ web-app/models/sequence.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose');
+
+// TODO: Use docker alias
+mongoose.connect('mongodb://localhost/proteinfinder');
+
+var db = mongoose.connection;
+
+// Sequence Schema
+var SequenceSchema = mongoose.Schema({
+    username: {
+      type: String,
+    },
+    query: {
+      type: String
+    },
+    protein: {
+      type: String
+    }
+  }, {
+    autoindex: true
+  }
+);
+
+var Sequence = module.exports = mongoose.model('Sequence', SequenceSchema);
+
+module.exports.createRecord = function(newSequence, callback) {
+   newSequence.save(callback);
+};
+
+module.exports.getRecords = function(username, callback) {
+  Sequence.find({ username: username }, callback);
+};
+
--- web-app/routes/index.js
+++ web-app/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var Sequence = require('../models/sequence');
+
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
@@ -11,6 +13,43 @@ function ensureAuthenticated(req, res, next) {
 /* GET home page. */
 router.get('/', ensureAuthenticated, function(req, res, next) {
   res.render('index', { title: 'Members' });
+});
+
+// Sequence query received?
+router.post('/sequence', function(req, res, next) {
+  console.log("yey, got a sequence");
+  console.log("Sequence is:", req.body.sequence);
+  console.log("Username is:", req.user.username);
+
+  // TODO: Some error handling first
+  var newSequence = new Sequence({
+    username: req.user.username,
+    query: req.body.sequence,
+    protein: ''
+  });
+
+  // TODO: Only ask to create if does not already exist, otherwise sorting gets mixed up too..
+  Sequence.createRecord(newSequence, function(err, sequence) {
+    if (err) throw err;
+
+    console.log("New sequence created:", sequence);
+
+    var dict = {};
+
+    Sequence.getRecords(req.user.username, function(err, users) {
+       var queries = [];
+       for (var i in users) {
+         key = users[i].query;
+         if (!(key in dict)) {
+            dict[key] = {};
+            dict[key]["queries"] = [];
+         }
+         dict[key]["queries"].push(users[i]._id);
+       }
+       console.log(dict);
+       res.render('index', { queries: users, user: req.user.username || null });
+    });
+  });
 });
 
 module.exports = router;
