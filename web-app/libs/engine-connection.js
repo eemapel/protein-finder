8c45abb91c3ebddf09e945bb92287db7de5934b6
Marko Pellikka <eemapel@gmail.com>
2017-11-13 03:49:21 +0000

    Create database records as they are received from bio-engine

--- web-app/libs/engine-connection.js
+++ web-app/libs/engine-connection.js
@@ -6,8 +6,11 @@ var portfinder = require('portfinder');
 var enableDestroy = require('server-destroy');
 var qs = require('querystring');
 
+var Sequence = require('../models/sequence');
+
 // Run custom server for each sequence query
-module.exports.connectionManager = function(callback) {
+module.exports.connectionManager = function(username, query, callback) {
+
   // Find open port on range 8000 ->
   portfinder.getPort(function (err, port) {
     if (err) throw err;
@@ -42,6 +45,18 @@ module.exports.connectionManager = function(callback) {
         res.end();
         if (req.method == "PUT") {
           console.log(qs.parse(data));
+
+          var newSequence = new Sequence({
+            username: username,
+            query: query,
+            protein: qs.parse(data)["reply"]
+          });
+
+          Sequence.createRecord(newSequence, function(err, sequence) {
+            if (err) throw err;
+            console.log("New sequence created:", sequence);
+          });
+
         } else if (req.method == "DELETE") {
           console.log("Delete received!");
           server.destroy();
--- web-app/routes/index.js
+++ web-app/routes/index.js
@@ -22,35 +22,28 @@ router.post('/sequence', function(req, res, next) {
   console.log("Username is:", req.user.username);
 
   // TODO: Some error handling first
-  var newSequence = new Sequence({
-    username: req.user.username,
-    query: req.body.sequence,
-    protein: ''
-  });
+  //var newSequence = new Sequence({
+  //  username: req.user.username,
+  //  query: req.body.sequence,
+  //  protein: ''
+  //});
 
   console.log("Start connection manager..");
-  CM.connectionManager(function() {
-    // TODO: Only ask to create if does not already exist, otherwise sorting gets mixed up too..
-    Sequence.createRecord(newSequence, function(err, sequence) {
-      if (err) throw err;
+  CM.connectionManager(req.user.username, req.body.sequence, function() {
+    var dict = {};
 
-      console.log("New sequence created:", sequence);
-
-      var dict = {};
-
-      Sequence.getRecords(req.user.username, function(err, users) {
-         var queries = [];
-         for (var i in users) {
-           key = users[i].query;
-           if (!(key in dict)) {
-              dict[key] = {};
-              dict[key].queries = [];
-           }
-           dict[key].queries.push(users[i]._id);
-         }
-         console.log(dict);
-         res.render('index', { queries: users, user: req.user.username || null });
-      });
+    Sequence.getRecords(req.user.username, function(err, users) {
+      var queries = [];
+        for (var i in users) {
+          key = users[i].query;
+          if (!(key in dict)) {
+            dict[key] = {};
+            dict[key].queries = [];
+          }
+          dict[key].queries.push(users[i]._id);
+       }
+       console.log(dict);
+       res.render('index', { queries: users, user: req.user.username || null });
     });
   });
 });
